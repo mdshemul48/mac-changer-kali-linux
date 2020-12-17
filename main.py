0ed2b3990c8a44cdc40ea01b95ef6ffc71e87293
MD. Shimul <mdshemul480@gmail.com>
2020-12-17 14:37:43 -0500

    added confirmations

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import optparse
+import subprocess
+import re
 
 
 def get_arguments():
@@ -19,8 +21,23 @@ def change_mac(interface, new_mac):
     os.system(f"sudo ifconfig {interface} down")
     os.system(f"sudo ifconfig {interface} hw ether {new_mac}")
     os.system(f"sudo ifconfig {interface} up")
-    print("Done")
+
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result[0]
+    else:
+        print("[-] Could not get mac address")
 
 
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print(f"current mac = {str(current_mac)}")
 change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print(f"Mac address successfully change to {str(current_mac)}")
+else:
+    print("[-] Mac address not changed..")
